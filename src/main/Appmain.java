8c2245aea54bcbcb5dac2800ac39202cab03b950
yoavs111 <yoav_s@info.cts.org.il>
2016-05-26 15:41:37 +0300

    check branch on main2

--- src/main/Appmain.java
+++ src/main/Appmain.java
@@ -9,7 +9,7 @@ public class Appmain {
 	public static void main(String[] args) {
 		AppFrame appFrame = new AppFrame();//// create appFrame
 		appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		appFrame.setSize(500, 550); // set frame size
+		appFrame.setSize(500, 500); // set frame size
 		appFrame.setVisible(true); // display frame
 	} // end main
 } // end class LabelTest
