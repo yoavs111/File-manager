44843a1a00cb5cb195f2f8c94e2868e733010013
yoav111 <yoav_s@info.cts.org.il>
2016-05-28 16:14:07 +0300

    try pull from leptop eclipse to home eclipse

--- src/managerOfGUI/AppFrame.java
+++ src/managerOfGUI/AppFrame.java
@@ -21,7 +21,7 @@ public class AppFrame extends JFrame {
 	private JButton renameButton;
 
 
-	private String textingArr[]={"test1","test2","test3","test4","test5","test6","test7","test8","test9"};
+	private String textingArr[]={"test1","test2","test3","test4","test5","test6","test7","test8"};
 	// fileds...
 
 	@SuppressWarnings("unchecked")
