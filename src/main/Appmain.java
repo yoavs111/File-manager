866b640f4566855c5e37df129216e11febec227d
yoavs111 <yoav_s@info.cts.org.il>
2016-05-29 16:59:20 +0300

    29.5.16

--- src/main/Appmain.java
+++ src/main/Appmain.java
@@ -9,7 +9,7 @@ public class Appmain {
 	public static void main(String[] args) {
 		AppFrame appFrame = new AppFrame();//// create appFrame
 		appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		appFrame.setSize(500, 500); // set frame size
+		appFrame.setSize(520, 350); // set frame size
 		//pack is used when you are using layout managers and you want the JFrame to be the smallest size showing all components. 
 		//If you set the size of your JFrame manually ( setSize(), setBounds() ) then you don't need to call pack().
 		//appFrame.pack();
